de58e9dc28fa16bd2f53cb7e49fe4a7b76a8ab0a
José Sánchez-Gallego <gallegoj@uw.edu>
2022-05-12 00:45:50 -0700

    Reprocess lvmieb outputs as part of hardware-status command

--- python/lvmscp/actor/commands/hardware_status.py
+++ python/lvmscp/actor/commands/hardware_status.py
@@ -15,6 +15,9 @@ if TYPE_CHECKING:
     from lvmscp.actor.actor import CommandType
 
 
+__all__ = ["hardware_status"]
+
+
 @parser.command()
 async def hardware_status(
     command: CommandType,
@@ -22,17 +25,22 @@ async def hardware_status(
 ):
     """Actor command that prints the status of the connected hardware."""
 
-    ieb_commands = ["wago status", "wago getpower", "transducer status"]
+    ieb_command_strings = ["wago status", "wago getpower", "transducer status"]
 
     for controller in controllers.values():
-        ieb_commands.append(f"hartmann status {controller.name}")
-        ieb_commands.append(f"shutter status {controller.name}")
-        ieb_commands.append(f"transducer status {controller.name}")
+        ieb_command_strings.append(f"hartmann status {controller.name}")
+        ieb_command_strings.append(f"shutter status {controller.name}")
+        ieb_command_strings.append(f"transducer status {controller.name}")
 
-    commands = [
-        await command.send_command("lvmieb", ieb_command, new_command=False)
-        for ieb_command in ieb_commands
+    ieb_commands = [
+        await command.send_command("lvmieb", ieb_command_string, new_command=True)
+        for ieb_command_string in ieb_command_strings
     ]
-    await asyncio.gather(*commands)
+    await asyncio.gather(*ieb_commands)
+
+    for ieb_command in ieb_commands:
+        for reply in ieb_command.replies:
+            if reply.message != {}:
+                command.info(message=reply.message)
 
     return command.finish()
