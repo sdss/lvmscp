7c9216b5ccc4bc91c1b55db5746764c86ef73ad3
José Sánchez-Gallego <gallegoj@uw.edu>
2022-05-14 12:13:02 -0700

    Cleanup tests, but basically placeholders

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,78 +1,30 @@
-# encoding: utf-8
-#
-# conftest.py
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-"""
-Here you can add fixtures that will be used for all the tests in this
-directory. You can also add conftest.py files in underlying subdirectories.
-Those conftest.py will only be applies to the tests in that subdirectory and
-underlying directories. See https://docs.pytest.org/en/2.7.3/plugins.html for
-more information.
-"""
+from __future__ import annotations
 
 import os
 
 import pytest as pytest
 
 import clu.testing
-from clu import LegacyActor
-from clu.actor import AMQPBaseActor
-from sdsstools import merge_config, read_yaml_file
+from sdsstools import read_yaml_file
 
-from lvmscp import config
-from lvmscp.actor.actor import lvmscp as ScpActor
+from lvmscp.actor import SCPActor
 
 
 @pytest.fixture()
 def test_config():
-
-    extra = read_yaml_file(os.path.join(os.path.dirname(__file__), "test_actor.yml"))
-    yield merge_config(extra, config)
+    yield read_yaml_file(os.path.join(os.path.dirname(__file__), "test_lvmscp.yml"))
 
 
 @pytest.fixture()
-async def actor(test_config: dict, mocker):
+async def actor(test_config: dict):
 
-    # We need to call the actor .start() method to force it to create the
-    # supervisors and to start the tasks, but we don't want to run .start()
-    # on the actor.
-
-    mocker.patch.object(AMQPBaseActor, "start")
-
-    _actor = ScpActor.from_config(test_config)
-
-    await _actor.start()
-
+    _actor = SCPActor.from_config(test_config)
     _actor = await clu.testing.setup_test_actor(_actor)  # type: ignore
 
     yield _actor
 
     _actor.mock_replies.clear()
-    await _actor.stop()
-
-
-@pytest.fixture()
-def test_config_lvmieb():
-
-    extra = read_yaml_file(os.path.join(os.path.dirname(__file__), "test_lvmieb.yml"))
-    yield merge_config(extra, config)
-
-
-@pytest.fixture()
-async def lvmieb(test_config_lvmieb: dict, mocker):
-
-    # We need to call the actor .start() method to force it to create the
-    # supervisors and to start the tasks, but we don't want to run .start()
-    # on the actor.
-
-    mocker.patch.object(AMQPBaseActor, "start")
-
-    _actor = await clu.testing.setup_test_actor(
-        LegacyActor("lvmieb", host="localhost", port=5672)
-    )  # type: ignore
-
-    yield _actor
-    await _actor.start()
-
-    # _actor.mock_replies.clear()
     await _actor.stop()
